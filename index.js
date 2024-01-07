01e8df0c83cdaa62890a93bd78e0206a31ffbd9c
YoruNoKen <yorunoken@skiff.com>
2024-01-07 15:28:49 +0300

    attemp djs

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const PORT = 4000
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
-})
+});
+
+app.get('/sendDiscordMessage', (req, res) => {
+  const message = 'Hello, this is your Express app sending a Discord message!';
+  discordBot.sendMessage(message);
+  res.send('Message sent!');
+});
 
 
 app.listen(PORT, () => {
