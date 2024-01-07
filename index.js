8eb2321e4939c2c431fb7c9cf981efefb0065c4d
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-07 21:36:48 +0300

    testinggg

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
-const { sendMessage } = require('./discordBot')
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
+async function sendMessage(message) {
+    return fetch(process.env.webhookURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+}
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+app.get("/home", (req, res) => {
+    res.status(200).json("Welcome, your app is working well");
 });
 
-app.get('/sendDiscordMessage', (req, res) => {
-  console.log(process.env.CLIENT_KEY)
-  console.log("wahhhh")
-  const message = 'Hello, this is your Express app sending a Discord message!';
-  sendMessage(message);
-  res.send('Message sent!');
-  console.log("wahhhh")
-});
+app.get("/send", (req, res) => {
+    const message =
+        "Hello, this is your Express app sending a Discord message!";
 
+    sendMessage(message).then((resolved) => {
+        res.send({
+            message: "You can now safely close this tab.",
+            resolved,
+        });
+    });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
-module.exports = app
+module.exports = app;
