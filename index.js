6e8d694df10df3f3aee426c1a8ef2adf39ffe5db
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-08 03:52:01 +0300

    did it fr this tiem

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { default: OsuStrategy } = require("passport-osu");
 const passport = require("passport");
 const session = require("express-session");
+require("dotenv/config");
 
 async function sendMessage(message) {
     fetch(process.env.webhookURL, {
@@ -12,34 +14,6 @@ async function sendMessage(message) {
             content: message,
         }),
     });
-}
-
-async function getUser(auth) {
-    try {
-        const token = await fetch(
-            `https://osu.ppy.sh/oauth/token?code=${auth}`,
-            {
-                method: "POST",
-            }
-        );
-        const { access_token } = await token.json();
-
-        const response = await fetch("https://osu.ppy.sh/api/v2/me/osu", {
-            method: "GET",
-            headers: { Authorization: `Bearer ${access_token}` },
-        });
-
-        if (!response.ok) {
-            throw new Error(
-                `API request failed with status ${response.status}`
-            );
-        }
-
-        return await response.json();
-    } catch (error) {
-        // Handle errors
-        console.error("Error making API request:", error.message);
-    }
 }
 
 class Server {
@@ -58,6 +32,33 @@ class Server {
         this.app.use(passport.initialize());
         this.app.use(passport.session());
 
+        const clientId = process.env.CLIENT_ID || "clientID";
+        const clientSecret = process.env.CLIENT_SECRET || "clientSecret";
+        const callbackUrl = "https://hanami-verifier.vercel.app/auth/osu/cb";
+
+        const strat = new OsuStrategy(
+            {
+                clientID: clientId,
+                clientSecret,
+                userProfileUrl: "https://osu.ppy.sh/api/v2/me/osu",
+                callbackURL: callbackUrl,
+            },
+            (_accessToken, _refreshToken, profile, cb) => {
+                // console.log(profile);
+                return cb(null, profile);
+            }
+        );
+
+        passport.use(strat);
+
+        passport.serializeUser((user, done) => {
+            done(null, user);
+        });
+
+        passport.deserializeUser((user, done) => {
+            done(null, user);
+        });
+
         this.app.get(
             "/auth/osu",
             (_req, _res, next) => {
@@ -66,17 +67,16 @@ class Server {
             passport.authenticate("osu")
         );
 
-        this.app.get("/auth/osu/cb", async (req, res) => {
-            const code = req.query.code;
-            const discordId = req.query.status;
-            const user = await getUser(code);
-
-            console.log(user);
-            sendMessage(`User fdfddf has logged in!\nDiscord ID: ${discordId}`);
-            res.json({
-                message: "You can now safely close this tab",
-            });
-        });
+        this.app.get(
+            "/auth/osu/cb",
+            passport.authenticate("osu", { failureRedirect: "/" }),
+            (req, res) => {
+                sendMessage(
+                    `Discord ID: ${req.query.state}\nosu! ID: ${req.user.id}`
+                );
+                res.send("You can now close this tab.");
+            }
+        );
 
         const host = "localhost";
         const port = process.env.PORT || 8000;
